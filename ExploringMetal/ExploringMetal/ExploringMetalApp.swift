e755b64c281245a7286e7df583662c1705fc079e
Wayne Dahlberg <waynefj40@gmail.com>
2024-03-11 18:52:45 -0600

    Initial commit

---
+++ ExploringMetal/ExploringMetal/ExploringMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringMetalApp.swift
+//  ExploringMetal
+//
+//  Created by Wayne Dahlberg on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ExploringMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
